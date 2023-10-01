5a936232327f2a479287c4a6210fa6299d6ed01a
Kevin S <146198846+2022271-KAS@users.noreply.github.com>
2023-10-01 17:06:43 +0100

    Update HelloWorld.java
    Displayed options to the user and a variable to store user's input.

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -11,7 +11,12 @@ public class HelloWorld {
         
         //added a Scanner, second commit.
         Scanner myKB = new Scanner(System.in);
-       
+
+        System.out.println("Press 1 to say: Hello, humans.");
+        System.out.println("or");
+        System.out.println("Press 2 to say: Goodbye.");
+        
+        int answer = myKB.nextInt();
     }
     
 }
